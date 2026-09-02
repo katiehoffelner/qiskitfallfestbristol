04506f8eac15bfe0b29df957d5cf568fadb3ac9d
Alexander Wood <alexljwood24@hotmail.co.uk>
2026-09-03 00:08:01 +0200

    fix pages action

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import { siteConfig } from './src/config/site.config'
 
+// add the base path for the gh pages project
+const isGithubPagesProjectSite = process.env.GITHUB_ACTIONS === 'true'
+const base = isGithubPagesProjectSite ? '/qiskitfallfestbristol' : '/'
+
 export default defineConfig({
   site: siteConfig.seo.siteUrl,
+  base,
   integrations: [sitemap()],
 })
