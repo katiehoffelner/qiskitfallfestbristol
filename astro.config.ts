2fc90bbb25a9f14f10b7c8577613cfe61c6e0123
Alexander Wood <alexljwood24@hotmail.co.uk>
2026-09-07 19:48:39 +0200

    make it work with the custom domain

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import { siteConfig } from './src/config/site.config'
 
-// add the base path for the gh pages project
-const isGithubPagesProjectSite = process.env.GITHUB_ACTIONS === 'true'
-const base = isGithubPagesProjectSite ? '/qiskitfallfestbristol' : '/'
+const base = '/'
 
 export default defineConfig({
   site: siteConfig.seo.siteUrl,
