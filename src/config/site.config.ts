d778800395f0a15d47173e5fbe7fd6581b8e1d2f
katiehoffelner <katie.hoffelner@outlook.com>
2026-09-11 15:41:27 +0100

    Update site.config.ts added a faq question

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -341,17 +341,33 @@ const siteConfigRaw = {
       {
         question: 'Do I need to know anything about quantum computing?',
         answer:
-          'No, our sessions are designed to provide value to everyone, regardless of prior knowledge. However, if you want to get more out of the event, we would recommend checking out our Resources page to learn more.',
+          'No, our sessions are designed to provide value to everyone, regardless of prior knowledge. \
+          However, if you want to get more out of the event, we would recommend checking out our Resources page to learn more.',
       },
       {
         question: 'I won\'t be available to attend all the sessions, is that okay?',
         answer:
-          'Attendance at all four sessions is recommended but not compulsory. Please register even if you only plan on attending one session. For those who can\'t attend the coding workshop but would still like to try out the exercises, our team will be available on our Discord server to help out.',
+          'Attendance at all four sessions is recommended but not compulsory. \
+          Please register even if you only plan on attending one session. \
+          For those who can\'t attend the coding workshop but would still like to try out the exercises, \
+          our team will be available on our Discord server to help out.',
       },
       {
         question: 'Do I need to prepare anything before the event?',
         answer:
-          'Our coding workshop will make use of the Qiskit library on Python, which will need to be installed. We highly encourage you to work through the Resources page which will help you making the necessary installations, so that you can spend more time during the session working on the coding exercises. The first three sessions don\'t require any preparation, just show up ready to learn!',
+          'Our coding workshop will make use of the Qiskit library on Python, \
+          which will need to be installed. \
+          We highly encourage you to work through the Resources page which will help you making the necessary installations, \
+          so that you can spend more time during the session working on the coding exercises. \
+          The first three sessions don\'t require any preparation, just show up ready to learn!',
+      },
+      {
+        question: 'Will IBM charge me to use their quantum computers?',
+        answer:
+          'IBM Quantum offers an "Open" plan, which gives 10 minutes of free computing time. \
+          You will not be charged if you use this; if your experiment goes over 10 minutes, \
+          they will stop computing rather than charging you. You will be charged if and only if you open a "Pay-as-you-go" instance, \
+          but this is not necessary to complete our coding workshop.',
       },
     ],
   },
